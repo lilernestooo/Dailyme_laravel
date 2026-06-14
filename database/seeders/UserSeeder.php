c7adfb109def7c6ec0378e3aab8770fd5b8861f4
lilernestooo <ernestlazatin69@gmail.com>
2026-06-14 12:32:34 +0800

    Refactor: seeder security

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,41 +14,46 @@ class UserSeeder extends Seeder
 
         $fixed = [
             [
-                'name'              => env('ADMIN_NAME', 'Admin User'),
-                'email'             => env('ADMIN_EMAIL', 'admin@example.com'),
+                'name'              => env('ADMIN_NAME'),
+                'email'             => env('ADMIN_EMAIL'),
                 'password'          => Hash::make(env('ADMIN_PASSWORD', $defaultPassword)),
                 'email_verified_at' => now(),
                 'is_admin'          => true,
             ],
             [
-                'name'              => env('QA_NAME', 'QA Engineer'),
-                'email'             => env('QA_EMAIL', 'qa@example.com'),
+                'name'              => env('QA_NAME'),
+                'email'             => env('QA_EMAIL'),
                 'password'          => Hash::make(env('QA_PASSWORD', $defaultPassword)),
                 'email_verified_at' => now(),
                 'is_admin'          => false,
             ],
             [
-                'name'              => env('DEV1_NAME', 'Developer One'),
-                'email'             => env('DEV1_EMAIL', 'dev1@example.com'),
+                'name'              => env('DEV1_NAME'),
+                'email'             => env('DEV1_EMAIL'),
                 'password'          => Hash::make(env('DEV1_PASSWORD', $defaultPassword)),
                 'email_verified_at' => now(),
                 'is_admin'          => false,
             ],
             [
-                'name'              => env('DEV2_NAME', 'Developer Two'),
-                'email'             => env('DEV2_EMAIL', 'dev2@example.com'),
+                'name'              => env('DEV2_NAME'),
+                'email'             => env('DEV2_EMAIL'),
                 'password'          => Hash::make(env('DEV2_PASSWORD', $defaultPassword)),
                 'email_verified_at' => now(),
                 'is_admin'          => false,
             ],
             [
-                'name'              => env('PM_NAME', 'Project Manager'),
-                'email'             => env('PM_EMAIL', 'pm@example.com'),
+                'name'              => env('PM_NAME'),
+                'email'             => env('PM_EMAIL'),
                 'password'          => Hash::make(env('PM_PASSWORD', $defaultPassword)),
                 'email_verified_at' => now(),
                 'is_admin'          => false,
             ],
         ];
+
+        // Skip any users where email is not set in env
+        $fixed = array_filter($fixed, function ($u) {
+            return !empty($u['email']);
+        });
 
         foreach ($fixed as $data) {
             User::firstOrCreate(['email' => $data['email']], $data);
