95badbfe7607be1bc170f97421c601ff19f2a348
lilernestooo <ernestlazatin69@gmail.com>
2026-06-14 09:52:57 +0800

    Fixed: db seeder changes

--- config/database.php
+++ config/database.php
@@ -47,11 +47,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_' . strtoupper(env('DB_CONFIG', 'local')) . '_HOST', '127.0.0.1'),
-            'port' => env('DB_' . strtoupper(env('DB_CONFIG', 'local')) . '_PORT', '3306'),
-            'database' => env('DB_' . strtoupper(env('DB_CONFIG', 'local')) . '_DATABASE', 'laravel'),
-            'username' => env('DB_' . strtoupper(env('DB_CONFIG', 'local')) . '_USERNAME', 'root'),
-            'password' => env('DB_' . strtoupper(env('DB_CONFIG', 'local')) . '_PASSWORD', ''),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -60,11 +60,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('DB_' . strtoupper(env('DB_CONFIG', 'local')) . '_SSL_CA') ?: null,
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => filter_var(
-                    env('DB_' . strtoupper(env('DB_CONFIG', 'local')) . '_SSL_VERIFY', true),
-                    FILTER_VALIDATE_BOOLEAN
-                ),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
 
